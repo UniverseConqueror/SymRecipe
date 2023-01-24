c3e9f7c79f2124fa68d5feedca0ccd34264f8989
UniverseConqueror <adrillef@gmail.com>
2023-01-24 17:20:48 +0100

    add home-page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page", methods={"GET"})
+     */
+    public function index(): Response
+    {
+       return $this->render('home.html.twig');
+    }
+
+
+}
+
+?>
